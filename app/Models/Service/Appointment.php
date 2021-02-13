8222e96ae5c9c6e570450d551be9af248c75c975
mostafafathi <mostafa.fathi.hilles@gmail.com>
2021-02-13 19:43:01 +0200

    d

--- app/Models/Service/Appointment.php
+++ app/Models/Service/Appointment.php
@@ -13,10 +13,14 @@ class Appointment extends Model
     protected $casts = ['times' => 'array','reserved_times' => 'array',
         'date' => 'date'];
     protected $hidden = ['created_at','date','reserved_times', 'updated_at', 'clinic_id', 'service_id'];
-    protected $appends = ['appointment_date'];
+    protected $appends = ['appointment_date','day_name'];
 
     public function getAppointmentDateAttribute()
     {
         return Carbon::parse($this->date)->format('d-m-Y');
+    }
+    public function getDayNameAttribute()
+    {
+        return Carbon::parse($this->date)->getTranslatedDayName();
     }
 }
