b191d774b388d8c0d9ead08f7835b32abe33fbb3
mostafafathi <mostafa.fathi.hilles@gmail.com>
2021-03-03 15:48:04 +0200

    d

--- app/Traits/SmsSender.php
+++ app/Traits/SmsSender.php
@@ -9,20 +9,30 @@ trait SmsSender
         $user = 'tjmel';
         $password = 'tjmel4313477';
         $sender = 'TJMEL';
-        $crl = curl_init('https://apps.gateway.sa/vendorsms/pushsms.aspx?user='.$user.'
-        &password='.$password.'
-        &msisdn='.$phoneNumber.'&sid='.$sender.'&msg='.$message.'&fl=0&dc=8');
-//        curl_setopt($crl, CURLOPT_RETURNTRANSFER, true);
-//        curl_setopt($crl, CURLINFO_HEADER_OUT, true);
-//        curl_setopt($crl, CURLOPT_POST, true);
-//        curl_setopt($crl, CURLOPT_POSTFIELDS, $post_data);
+        $data = array(
+            "user" => $user,
+            "password" => $password,
+            "sid" => $sender,
+            "msisdn" => $phoneNumber,
+            "msg" => $message,
+            "fl" => '0',
+            "dc" => '8',
+        );
+        $post_data = json_encode($data);
+        $crl = curl_init('https://apps.gateway.sa/vendorsms/pushsms.aspx');
+//        ?user='.$user.'
+//    &password='.$password.'
+//    &msisdn='.$phoneNumber.'&sid='.$sender.'&msg='.$message.'&fl=0&dc=8
+        curl_setopt($crl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($crl, CURLINFO_HEADER_OUT, true);
+        curl_setopt($crl, CURLOPT_POST, true);
+        curl_setopt($crl, CURLOPT_POSTFIELDS, $post_data);
 
         // Set HTTP Header for POST request
-//        curl_setopt($crl, CURLOPT_HTTPHEADER, array(
-//                'authorization: STJN99NK6Z-JBDZL26RHN-N2RJBBJWTD',
-//                'content-type: application/json',
-//            )
-//        );
+        curl_setopt($crl, CURLOPT_HTTPHEADER, array(
+                'Content-type: application/x-www-form-urlencoded\r\n',
+            )
+        );
         // Submit the POST request
         $result = curl_exec($crl);
 
