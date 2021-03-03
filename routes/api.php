4e192c9b0e8d54a7a02a7d0c9a2d3ca55a3cbcbc
mostafafathi <mostafa.fathi.hilles@gmail.com>
2021-03-03 16:08:34 +0200

    d

--- routes/api.php
+++ routes/api.php
@@ -1,56 +1,105 @@
-<?
-function PostRequest($url, $referer, $_data) {
-    // convert variables array to string:
-    $data = array();
-    while(list($n,$v) = each($_data)){
-        $data[] = "$n=$v";
-    }
-    $data = implode('&', $data);
-    // format --> test1=a&test2=b etc.
-    // parse the given URL
-    $url = parse_url($url);
-    if ($url['scheme'] != 'http') {
-        die('Only HTTP request are supported !');
-    }
-    // extract host and path:
-    $host = $url['host'];
-    $path = $url['path'];
-    // open a socket connection on port 80
-    $fp = fsockopen($host, 80);
-    // send the request headers:
-    fputs($fp, "POST $path HTTP/1.1\r\n");
-    fputs($fp, "Host: $host\r\n");
-    fputs($fp, "Referer: $referer\r\n");
-    fputs($fp, "Content-type: application/x-www-form-urlencoded\r\n");
-    fputs($fp, "Content-length: ". strlen($data) ."\r\n");
-    fputs($fp, "Connection: close\r\n\r\n");
-    fputs($fp, $data);
-    $result = '';
-    while(!feof($fp)) {
-        // receive the results of the request
-        $result .= fgets($fp, 128);
-    }
-    // close the socket connection:
-    fclose($fp);
-    // split the result header from the content
-    $result = explode("\r\n\r\n", $result, 2);
-    $header = isset($result[0]) ? $result[0] : '';
-    $content = isset($result[1]) ? $result[1] : '';
-    // return as array:
-    return array($header, $content);
-}
-$data = array(
-    'user' => "user",
-    'password' => "password",
-    'msisdn' => "966560001716",
-    'sid' => "Gateway.sa",
-    'msg' => "Test Message from Gateway.sa",
-    'fl' =>"0",
-);
-list($header, $content) = PostRequest(
-    "http://184.95.57.221:8080/vendorsms/pushsms.aspx?user=abc&password=xyz&msisdn=966500xxxxxx&sid=SenderId&msg=test%20message&fl=0&gwid=2 " // the url to post to
-    , “”,
-    $data
-);
-echo $content;
-?>
+<?php
+
+use App\Http\Controllers\Api\ClinicController;
+use App\Http\Controllers\Api\FavoriteController;
+use App\Http\Controllers\Api\OfferController;
+use App\Http\Controllers\Api\ReservationController;
+use App\Http\Controllers\Api\ServiceController;
+use App\Http\Controllers\Api\UserAddressController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\SettingController;
+use App\Http\Controllers\Api\CartController;
+use App\Http\Controllers\Api\OrderController;
+use App\Http\Controllers\Api\Auth\RegisterController;
+use App\Http\Controllers\Api\Auth\LoginController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+*/
+
+Route::group(['middleware' => ['localization', 'auth:sanctum', 'throttle:api'], 'as' => 'api.'], function () {
+
+
+    Route::group(['prefix' => 'user', 'as' => 'user.'], function () {
+        /* one route calls */
+        Route::get('/', [UserController::class, 'showUserProfile'])->name('show');
+        Route::post('/', [UserController::class, 'updateUserProfile'])->name('update');
+        Route::get('/reservations', [ReservationController::class, 'showUserReservations'])->name('reservations');
+        Route::get('/wallet', [UserController::class, 'getUserWallet'])->name('wallet');
+        Route::group(['prefix' => 'address', 'as' => 'address.'], function () {
+            /* one route calls */
+            Route::get('/', [UserAddressController::class, 'showUserAddresses'])->name('show');
+            Route::post('/', [UserAddressController::class, 'storeUserAddress'])->name('store');
+            Route::post('/{id}', [UserAddressController::class, 'updateUserAddress'])->name('update');
+            Route::delete('/{id}', [UserAddressController::class, 'destroyUserAddress'])->name('destroy');
+            Route::post('/{id}/current', [UserAddressController::class, 'makeUserAddressAsCurrent'])->name('current');
+        });
+    });
+    Route::group(['prefix' => 'favorite', 'as' => 'favorite.'], function () {
+        /* one route calls */
+        Route::get('/', [FavoriteController::class, 'showUserFavorites'])->name('index');
+        Route::post('/', [FavoriteController::class, 'storeUserFavorite'])->name('store');
+        Route::post('/remove', [FavoriteController::class, 'destroyUserFavorite'])->name('destroy');
+    });
+    Route::group(['prefix' => 'clinic', 'as' => 'clinic.'], function () {
+        /* one route calls */
+        Route::get('/rating', [ClinicController::class, 'showClinicsByRating'])->name('rating.order');
+        Route::get('/location', [ClinicController::class, 'showClinicsByLocation'])->name('location.order');
+        Route::get('/search', [ClinicController::class, 'searchClinics'])->name('search');
+        Route::get('/{id}', [ClinicController::class, 'showClinic'])->name('show');
+        Route::post('/{id}/rate', [ClinicController::class, 'rateClinic'])->name('rate');
+        Route::post('/{id}/service/{service_id}/reserve', [ClinicController::class, 'reserveClinicService'])->name('service.reserve');
+        Route::post('/{id}/offer/{offer_id}/reserve', [ClinicController::class, 'reserveClinicOffer'])->name('offer.reserve');
+    });
+    Route::group(['prefix' => 'reserve', 'as' => 'reserve.'], function () {
+        /* one route calls */
+        Route::post('/{id}/cancel', [ReservationController::class, 'cancelReserve'])->name('cancel');
+        Route::get('/{id}/save', [ReservationController::class, 'saveReserve'])->name('save');
+    });
+    Route::group(['prefix' => 'offer', 'as' => 'offer.'], function () {
+        /* one route calls */
+        Route::get('/price', [OfferController::class, 'showOffersByPrice'])->name('price.order');
+        Route::get('/location', [OfferController::class, 'showOffersByLocation'])->name('location.order');
+        Route::get('/{id}', [OfferController::class, 'showOffer'])->name('show');
+    });
+    Route::group(['prefix' => 'service', 'as' => 'service.'], function () {
+        /* one route calls */
+        Route::get('/price', [ServiceController::class, 'showServicesByPrice'])->name('price.order');
+        Route::get('/location', [ServiceController::class, 'showServicesByLocation'])->name('location.order');
+        Route::get('/{id}', [ServiceController::class, 'showService'])->name('show');
+    });
+
+});
+
+/* Start General routes */
+Route::group(['as' => 'api.', 'middleware' => ['localization', 'throttle:api']], function () {
+    /* one route calls */
+    Route::post('/register/otp/send', [RegisterController::class, 'sendOtpCode'])->name('register.otp.send');
+    Route::post('/login/otp/send', [LoginController::class, 'sendOtpCode'])->name('login.otp.send');
+    Route::post('/user/otp/verify', [UserController::class, 'userVerifyOtpCode'])->name('user.otp.verify');
+    Route::post('/clinic/application', [ClinicController::class, 'storeClinicRequest'])->name('application.store');
+
+    Route::group(['prefix' => 'setting', 'as' => 'setting.'], function () {
+        /* one route calls */
+        Route::get('/about_us', [SettingController::class, 'showAboutUs'])->name('about_us');
+        Route::get('/agreement', [SettingController::class, 'showAgreement'])->name('agreement');
+        Route::post('/contact_us', [SettingController::class, 'storeContactUs'])->name('contact_us');
+        Route::get('/advertisements', [SettingController::class, 'getAdvertisement'])->name('advertisements');
+        Route::get('/', [SettingController::class, 'showSettings'])->name('show');
+        Route::get('/cities', [SettingController::class, 'showCities'])->name('cities.show');
+
+    });
+    /* redirect not logged-in users to unauthenticated route */
+    Route::get('/unauthenticated', function () {
+        return response()->json(['message' => 'unauthenticated', 'status' => false], 401);
+    })->name('unauthenticated');
+});
+/* End General routes */
+
