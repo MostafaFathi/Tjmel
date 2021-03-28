2f4061687e79067b3d5e5f55dce156d561f102da
mostafafathi <mostafa.fathi.hilles@gmail.com>
2021-03-28 15:54:29 +0300

    d

--- app/Http/Controllers/Clinic/ReservationController.php
+++ app/Http/Controllers/Clinic/ReservationController.php
@@ -16,12 +16,12 @@ class ReservationController extends Controller
      */
     public function index()
     {
-        $todayReservations = auth()->user()->clinic->reservations->where('status',5)->where('appointment_date',Carbon::today()->format('d-m-Y'));
+        $todayReservations = auth()->user()->clinic->reservations->where('status',5)->where('appointment_date',Carbon::today()->format('d-m-Y'))->orderBy('id','desc');
 //        $nowReservations = Reserve::where('clinic_id',auth()->user()->clinic->id)->where('status',5)->where('appointment_date',Carbon::today()->format('d-m-Y'))
 //            ->whereBetween('appointment_time',[Carbon::now()->subMinutes(30),Carbon::now()])->get();
-        $comingReservations = Reserve::where('clinic_id',auth()->user()->clinic->id)->where('status',5)->get();//->wheredate('appointment_date','>',Carbon::today())
-        $completedReservations = auth()->user()->clinic->reservations->where('status',1);
-        $unCompletedReservations = auth()->user()->clinic->reservations->whereIn('status',[2,3,4]);
+        $comingReservations = Reserve::where('clinic_id',auth()->user()->clinic->id)->where('status',5)->orderBy('id','desc')->get();//->wheredate('appointment_date','>',Carbon::today())
+        $completedReservations = auth()->user()->clinic->reservations->where('status',1)->orderBy('id','desc');
+        $unCompletedReservations = auth()->user()->clinic->reservations->whereIn('status',[2,3,4])->orderBy('id','desc');
         return view('clinic.reservations.index',compact('todayReservations','comingReservations','completedReservations','unCompletedReservations'));
     }
 
