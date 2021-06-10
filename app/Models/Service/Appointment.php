0a4979ebe8bccefdcb1f9f6c704b415e788bd6a9
mostafafathi <mostafa.fathi.hilles@gmail.com>
2021-06-10 15:03:06 +0300

    d

--- app/Models/Service/Appointment.php
+++ app/Models/Service/Appointment.php
@@ -10,15 +10,16 @@ class Appointment extends Model
 {
     use HasFactory;
 
-    protected $casts = ['times' => 'array','reserved_times' => 'array',
+    protected $casts = ['times' => 'array', 'reserved_times' => 'array',
         'date' => 'date'];
-    protected $hidden = ['created_at','date','reserved_times', 'updated_at', 'clinic_id', 'service_id'];
-    protected $appends = ['appointment_date','day_name'];
+    protected $hidden = ['created_at', 'date', 'reserved_times', 'updated_at', 'clinic_id', 'service_id'];
+    protected $appends = ['appointment_date', 'day_name'];
 
     public function getAppointmentDateAttribute()
     {
         return Carbon::parse($this->date)->format('d-m-Y');
     }
+
     public function getDayNameAttribute()
     {
         return Carbon::parse($this->date)->getTranslatedDayName();
@@ -26,14 +27,15 @@ class Appointment extends Model
 
     public function getTimesAttribute($value)
     {
-        $value = json_decode($value,true);
+        $value = json_decode($value, true);
         $newArray = [];
-        if ($value != null and !empty($value)){
-            foreach ($value  as $key => $item) {
+        if ($value != null and !empty($value)) {
+            foreach ($value as $key => $item) {
                 $myTime = Carbon::createFromFormat('Y-m-d H:i a', $this->date->format('Y-m-d') . ' ' . $item['time']);
 
-                if ($item['status'] != "reserved" and $myTime > Carbon::now()->addHour())
-                    array_push($newArray,$item);
+                if (explode('.', request()->route()->action['as'])[0] == 'api')
+                    if ($item['status'] != "reserved" and $myTime > Carbon::now()->addHour())
+                        array_push($newArray, $item);
             }
         }
         return $newArray;
