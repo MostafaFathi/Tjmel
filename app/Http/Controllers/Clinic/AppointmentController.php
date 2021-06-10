800919e4e758c8e10467736fbbd2af5e8537a3f0
mostafafathi <mostafa.fathi.hilles@gmail.com>
2021-06-10 14:44:37 +0300

    d

--- app/Http/Controllers/Clinic/AppointmentController.php
+++ app/Http/Controllers/Clinic/AppointmentController.php
@@ -57,9 +57,10 @@ class AppointmentController extends Controller
                 foreach ($appointments as $appointment) {
 
                     if (explode(' ', $appointment->date)[0] == $month . '-' . str_pad($i, 2, '0', STR_PAD_LEFT)) {
-                        dd($selectedTimes);
+
                         $flag = true;
                         array_push($selectedTimes, $appointment->times);
+                        dd($selectedTimes,$appointment->times);
                         $monthList .= '<li class="day-btn checked" date="' . $currentMonth . '-' . $i . '"><input type="hidden" class="day-date" name="dates[]" value="' . $currentMonth . '-' . $i . '">' . $i . '<span>' . Carbon::parse($currentMonth . '-' . $i)->translatedFormat('l') . '</span></li>';
                     }
                 }
